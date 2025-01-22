c72218451b6ab89ea2d54f9902983c087b592e84
fabianRivero <fabianarj467@gmail.com>
2025-01-22 08:23:44 -0400

    ultimo commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@ import { defineConfig } from 'astro/config';
 import dotenv from 'dotenv';
 import react from "@astrojs/react";
 import node from '@astrojs/node';
+import netlify from '@astrojs/netlify/functions';
 
 dotenv.config();
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: node({ mode: 'standalone' }),
+  adapter: netlify(),
   integrations:[react()],
   server: {
     proxy: {
